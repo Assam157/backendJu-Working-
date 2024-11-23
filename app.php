c65dc54b3b66aefbd577ef6ad7a74a9f04287eb8
Assam157 <asasm8106@gmail.com>
2024-11-23 17:54:17 +0530

    Update app.php

--- app.php
+++ app.php
@@ -214,7 +214,7 @@
  $app->addBodyParsingMiddleware();
 //MongoDB connection
  $mongoClient = new MongoDB\Client(
-     "mongodb+srv://Maitreya:killdill12@cluster0.sk6ugig.mongodb.net/?retryWrites=true&w=majority",
+     "mongodb+srv://Maitreya:killdill12@cluster0.sk6ugig.mongodb.net/?retryWrites=true",
      [],
      [
          'typeMap' => [
